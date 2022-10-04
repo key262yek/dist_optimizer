7c71661b56852c4b80d84dd3cce5960a19450d91
Myeongseon Choi <key262yek@gmail.com>
2022-10-04 09:30:10 +0900

    Initialization

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+// Distribution Optimizer library
+// When we have to optimize a functional which maps a distribution into real number by chaning distribution
+// we can use this library.
+
+pub mod convert;
